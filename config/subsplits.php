3147726b9f895527e69e97928df33a7a086257b1
Robert Korulczyk <robert@korulczyk.pl>
2019-09-19 11:30:28 +0200

    Add subsplit for Turkish language extensions.

--- config/subsplits.php
+++ config/subsplits.php
@@ -9,6 +9,13 @@
  * with this source code in the file LICENSE.
  */
 
+$getProjectComponents = static function (string $projectId): array {
+	$keys = array_keys(require __DIR__ . "/$projectId-project.php");
+	return array_filter($keys, static function (string $key) {
+		return strncmp($key, '__', 2) !== 0;
+	});
+};
+
 return [
 	'pl' => [
 		'type' => 'language',
@@ -17,5 +24,28 @@ return [
 		'repository' => 'git@github.com:rob006-software/flarum-lang-polish.git',
 		'branch' => 'master',
 		'path' => '/locale',
+	],
+	'tr-core' => [
+		'type' => 'language',
+		'language' => 'tr',
+		'updateReadme' => true,
+		'repository' => 'git@github.com:tolgaaaltas/flarum-lang-turkish.git',
+		'branch' => 'collective',
+		'path' => '/locale',
+		'components' => [
+			'flarum' => $getProjectComponents('flarum'),
+		],
+	],
+	'tr-community' => [
+		'type' => 'language',
+		'language' => 'tr',
+		'updateReadme' => true,
+		'repository' => 'git@github.com:tolgaaaltas/flarum-ext-turkish.git',
+		'branch' => 'collective',
+		'path' => '/locale',
+		'components' => [
+			'fof' => $getProjectComponents('fof'),
+			'various' => $getProjectComponents('various'),
+		],
 	],
 ];
