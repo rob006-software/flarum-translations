fe0fa55ec72387c8ff92ab5644f3a4fb914190b1
Robert Korulczyk <robert@korulczyk.pl>
2019-12-20 21:43:58 +0100

    Migrate flagrow-html-errors to fof-html-errors

--- config/fof-project.php
+++ config/fof-project.php
@@ -33,6 +33,7 @@ return [
 	'fof-gamification' => 'https://raw.githubusercontent.com/FriendsOfFlarum/gamification/master/resources/locale/en.yml',
 	'fof-geoip' => 'https://raw.githubusercontent.com/FriendsOfFlarum/geoip/master/resources/locale/en.yml',
 	'fof-github-sponsors' => 'https://raw.githubusercontent.com/FriendsOfFlarum/github-sponsors/master/locale/en.yml',
+	'fof-html-errors' => 'https://raw.githubusercontent.com/FriendsOfFlarum/html-errors/master/locale/en.yml',
 	'fof-ignore-users' => 'https://raw.githubusercontent.com/FriendsOfFlarum/ignore-users/master/resources/locale/en.yml',
 	'fof-linguist' => 'https://raw.githubusercontent.com/FriendsOfFlarum/linguist/master/resources/locale/en.yml',
 	'fof-links' => 'https://raw.githubusercontent.com/FriendsOfFlarum/links/master/locale/en.yml',
