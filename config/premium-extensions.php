f236fb5481dd24e68b5344d2880433f08c6999de
Robert Korulczyk <robert@korulczyk.pl>
2020-05-25 23:28:36 +0200

    Add support for premium extensions.
    refs https://github.com/rob006-software/flarum-translations/issues/306
    refs https://github.com/rob006-software/flarum-translations-builder/issues/12

---
+++ config/premium-extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the flarum-translations-builder.
+ *
+ * Copyright (c) 2020 Robert Korulczyk <robert@korulczyk.pl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+return [
+];
