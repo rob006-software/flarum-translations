da7e5d7e76c032c4b478694e0baeffe7e4b1f7da
Robert Korulczyk <robert@korulczyk.pl>
2021-02-21 15:01:43 +0100

    Remove dem13n-auth-vkontakte.

--- config/components.php
+++ config/components.php
@@ -129,9 +129,6 @@ return [
 	'datitisev-maintenance' => [
 		'tag' => 'https://raw.githubusercontent.com/extiverse/premium-translations/master/datitisev-maintenance.yml',
 	],
-	'dem13n-auth-vkontakte' => [
-		'tag' => 'https://raw.githubusercontent.com/Dem13n/auth-vkontakte/0.1.1/resources/locale/en.yml',
-	],
 	'dem13n-auth-yandex' => [
 		'tag' => 'https://raw.githubusercontent.com/Dem13n/auth-yandex/0.1.0/resources/locale/en.yml',
 	],
