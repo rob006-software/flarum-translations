233d1d87acf4843307e4f35bc46d1c33472771e5
Robert Korulczyk <robert@korulczyk.pl>
2021-02-21 15:01:52 +0100

    Remove flagrow-fonts.

--- config/components.php
+++ config/components.php
@@ -138,9 +138,6 @@ return [
 	'dem13n-topic-starter-label' => [
 		'tag' => 'https://raw.githubusercontent.com/Dem13n/topic-starter-label/0.1.3/locale/en.yml',
 	],
-	'flagrow-fonts' => [
-		'tag' => 'https://raw.githubusercontent.com/flagrow/fonts/0.2.0/resources/locale/en.yml',
-	],
 	'flarum-akismet' => [
 		'tag' => 'https://raw.githubusercontent.com/flarum/lang-english/v0.1.0-beta.15/locale/flarum-akismet.yml',
 		'branch' => 'https://raw.githubusercontent.com/flarum/akismet/master/locale/en.yml',
