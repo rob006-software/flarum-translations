09a6e7513457b736f4665efd35a0c764c5a9e913
Robert Korulczyk <robert@korulczyk.pl>
2021-02-21 14:51:44 +0100

    Remove clarkwinkelmann-status.

--- config/components.php
+++ config/components.php
@@ -117,9 +117,6 @@ return [
 	'clarkwinkelmann-see-past-first-post' => [
 		'tag' => 'https://raw.githubusercontent.com/clarkwinkelmann/flarum-ext-see-past-first-post/1.1.0/resources/locale/en.yml',
 	],
-	'clarkwinkelmann-status' => [
-		'tag' => 'https://raw.githubusercontent.com/clarkwinkelmann/flarum-ext-status/0.1.1/resources/locale/en.yml',
-	],
 	'clarkwinkelmann-who-read' => [
 		'tag' => 'https://raw.githubusercontent.com/clarkwinkelmann/flarum-ext-who-read/1.2.2/resources/locale/en.yml',
 	],
