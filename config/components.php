fd1dd07ae6868edbcb542d9060f48e8c9079f42c
Robert Korulczyk <robert@korulczyk.pl>
2021-07-30 22:09:38 +0200

    Remove `shriker/flarum-pronouns` (incompatible).

--- config/components.php
+++ config/components.php
@@ -604,9 +604,6 @@ return [
 	'serakoi-flarumdiscordtheme' => [
 		'tag' => 'https://raw.githubusercontent.com/Serakoi/flarum-discord-theme-2/0.1.0/resources/locale/en.yml',
 	],
-	'shriker-pronouns' => [
-		'tag' => 'https://raw.githubusercontent.com/shriker/flarum-pronouns/0.1.3/resources/locale/en.yml',
-	],
 	'simonxeko-follow-users' => [
 		'tag' => 'https://raw.githubusercontent.com/simonxeko/follow-users/0.3.5/resources/locale/en.yml',
 	],
