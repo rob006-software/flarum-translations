93bac1582869aab21ecfe6666877da004d14bb67
Robert Korulczyk <robert@korulczyk.pl>
2022-08-30 13:08:11 +0200

    Enable release generator for Lithuanian language pack.

--- config/subsplits.php
+++ config/subsplits.php
@@ -220,6 +220,11 @@ return [
 		'repository' => 'git@github.com:flarum-lang/lithuanian.git',
 		'branch' => 'main',
 		'path' => '/locale',
+		'releaseGenerator' => [
+			'class' => ReleaseGenerator::class,
+			'versionTemplate' => 'Major.Minor.Patch',
+		],
+		'maintainers' => ['tavoweb'],
 	],
 	'lv' => [
 		'type' => 'language',
