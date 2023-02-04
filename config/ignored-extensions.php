a3ca917046f11c9fdbe0ffab3bc75cc2f365a831
Robert Korulczyk <robert@korulczyk.pl>
2023-02-04 20:28:16 +0100

    Remove `whydesd/reply-to-see` from ignored extensions - repo was removed from GitHub.
    refs https://github.com/rob006-software/flarum-translations/pull/775

--- config/ignored-extensions.php
+++ config/ignored-extensions.php
@@ -38,7 +38,6 @@ return [
 	'subasteve/flarum-ext-hljs',
 	'subasteve/flarum-ext-mailing',
 	'swaggymacro/websocket',
-	'whydesd/reply-to-see',
 	'william0wang/flarum-imgur-upload',
 	'zouyonghao/flarum-es',
 	/*
