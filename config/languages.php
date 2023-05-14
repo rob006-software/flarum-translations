663e9ee2a850ba22e935e661950516366cf7c883
Robert Korulczyk <robert@korulczyk.pl>
2023-05-14 13:56:27 +0200

    Add Nepali language.

--- config/languages.php
+++ config/languages.php
@@ -77,6 +77,7 @@ return [
 	'lt' => getComponents(),
 	'lv' => getComponents(),
 	'ml' => getComponents(),
+	'ne' => getComponents(),
 	'nl' => getComponents([
 		// translations included in extension
 		'!v17development-seo',
