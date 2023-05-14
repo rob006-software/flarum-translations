21ad5879f2e9632f8b2818a80d1ac9c3775712d2
Robert Korulczyk <robert@korulczyk.pl>
2023-05-14 13:57:52 +0200

    Add subsplit for Norwegian Nynorsk language pack.

--- config/subsplits.php
+++ config/subsplits.php
@@ -353,6 +353,18 @@ return [
 		],
 		'maintainers' => ['jaspervriends', 'MichaelBelgium'],
 	],
+	'nn' => [
+		'type' => 'language',
+		'language' => 'nn',
+		'repository' => 'git@github.com:flarum-lang/norwegian-nynorsk.git',
+		'branch' => 'master',
+		'path' => '/locale',
+		'releaseGenerator' => [
+			'class' => ReleaseGenerator::class,
+			'versionTemplate' => 'Major.Minor.Patch',
+		],
+		'maintainers' => [],
+	],
 	'pl' => [
 		'type' => 'language',
 		'language' => 'pl',
