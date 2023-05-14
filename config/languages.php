9f30b4d206934b9bbd5b7cce6e7d9545e82b3b99
Robert Korulczyk <robert@korulczyk.pl>
2023-05-14 13:57:52 +0200

    Add Norwegian Nynorsk language.

--- config/languages.php
+++ config/languages.php
@@ -81,6 +81,7 @@ return [
 		// translations included in extension
 		'!v17development-seo',
 	]),
+	'nn' => getComponents(),
 	'pl' => getComponents([
 		// translations included in extension
 		'!rob006-last-post-avatar',
