945f1fd2cdd1a4f03a790ef3168b047eaf5191f9
Robert Korulczyk <robert@korulczyk.pl>
2026-01-13 21:59:19 +0100

    Ignore `capybash/*` extensions

--- config/ignored-extensions.php
+++ config/ignored-extensions.php
@@ -13,6 +13,10 @@ declare(strict_types=1);
 
 return [
 	'zaptech/cookie-consent', // migrated to `fof/cookie-consent`
+	// migrated to `forumaker/*` - https://github.com/rob006-software/flarum-translations/pull/1445
+	'capybash/magicbb',
+	'capybash/magicread',
+	'capybash/magicslider',
 	/*
 	 * dead/private forks
 	 */
