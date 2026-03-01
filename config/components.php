b41b0087fd24146c82c96ec31295dcb3732de4be
Robert Korulczyk <robert@korulczyk.pl>
2026-03-01 23:01:15 +0100

    Remove Turkish language from built-in languages for `huseyinfiliz/flarum-diff`

--- config/components.php
+++ config/components.php
@@ -280,9 +280,6 @@ return [
 	],
 	'huseyinfiliz-diff' => [
 		'beta' => 'https://raw.githubusercontent.com/huseyinfiliz/flarum-diff/2.0.0-beta.4/locale/en.yml',
-		'__builtInLanguages' => [
-			'tr',
-		],
 	],
 	'huseyinfiliz-modern-footer' => [
 		'beta' => 'https://raw.githubusercontent.com/huseyinfiliz/modern-footer/v2.0.0-beta.1/resources/locale/en.yml',
