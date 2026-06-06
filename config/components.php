9d2aafecc0d3bdac3631c6c55cb34922f8a0a002
Robert Korulczyk <robert@korulczyk.pl>
2026-06-06 22:54:33 +0200

    Remove built-in languages for `ernestdefoe/digest-mail`
    They're inherited from forked repository and already outdated.

--- config/components.php
+++ config/components.php
@@ -127,9 +127,6 @@ return [
 	],
 	'ernestdefoe-digest-mail' => [
 		'tag' => 'https://raw.githubusercontent.com/ernestdefoe/digest-mail/2.0.1/locale/en.yml',
-		'__builtInLanguages' => [
-			'fr',
-		],
 	],
 	'ernestdefoe-facebook-post' => [
 		'tag' => 'https://raw.githubusercontent.com/ernestdefoe/flarum-facebook-post/v2.1.5/locale/en.yml',
