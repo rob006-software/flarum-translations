d276387967abd5871cf283fb04f201c0a41a0bf2
Robert Korulczyk <robert@korulczyk.pl>
2026-07-26 22:17:24 +0200

    [1.x] Remove `glowingblue/redis-setup`

--- config/components.php
+++ config/components.php
@@ -904,9 +904,6 @@ return [
 			'tr',
 		],
 	],
-	'glowingblue-redis-setup' => [
-		'tag' => 'https://raw.githubusercontent.com/glowingblue/flarum-ext-redis-setup/1.3.2/resources/locale/en.yml',
-	],
 	'gtdxyz-badges' => [
 		'tag' => 'https://raw.githubusercontent.com/daocatt/flarum-ext-badges/1.0.1/locale/en.yml',
 		'__builtInLanguages' => [
